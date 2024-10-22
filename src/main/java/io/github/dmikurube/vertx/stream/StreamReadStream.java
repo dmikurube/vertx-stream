bb1c393481a0b958300ec5a8a309e29d7d108f6d
Dai MIKURUBE <dmikurube@acm.org>
2024-10-22 22:02:11 +0900

    Add StreamReadStream

---
+++ src/main/java/io/github/dmikurube/vertx/stream/StreamReadStream.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 Dai MIKURUBE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.dmikurube.vertx.stream;
+
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.streams.ReadStream;
+
+public final class StreamReadStream implements ReadStream<Buffer> {
+    @Override
+    public ReadStream<Buffer> exceptionHandler(final Handler<Throwable> handler) {
+        return this;
+    }
+
+    @Override
+    public ReadStream<Buffer> handler(final Handler<Buffer> handler) {
+        return this;
+    }
+
+    @Override
+    public ReadStream<Buffer> pause() {
+        return this;
+    }
+
+    @Override
+    public ReadStream<Buffer> resume() {
+        return this;
+    }
+
+    @Override
+    public ReadStream<Buffer> fetch(final long amount) {
+        return this;
+    }
+
+    @Override
+    public ReadStream<Buffer> endHandler(final Handler<Void> endHandler) {
+        return this;
+    }
+}
